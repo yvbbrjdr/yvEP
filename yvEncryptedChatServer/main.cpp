87df5436e72cf80b2241365e5133b2772354a3fe
yvbbrjdr <xyyqzy@hotmail.com>
2016-03-16 18:12:08 +0800

    Add outputs to yvECS

--- yvEncryptedChatServer/main.cpp
+++ yvEncryptedChatServer/main.cpp
@@ -5,12 +5,18 @@
 #include <cstdlib>
 
 int main(int argc, char *argv[]) {
-    if (argc!=2) {
-        printf("Usage: %s <Port>\n",argv[0]);
+    QCoreApplication a(argc, argv);
+    if (argc>2) {
+        printf("Usage: %s [Port]\n",argv[0]);
         exit(0);
     }
-    QCoreApplication a(argc, argv);
-    MainThread thread(QString(argv[1]).toShort());
+    unsigned short Port=2428;
+    if (argc==2)
+        Port=QString(argv[1]).toShort();
+    printf("Welcome to yvEncryptedChatServer\n\nServer will be listening at Port %d\nGenerating RSA Key. . . ",Port);
+    fflush(stdout);
+    MainThread thread(Port);
     thread.start();
+    puts("Done!\n\nEnter nickname to logout");
     return a.exec();
 }
